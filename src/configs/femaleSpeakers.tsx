153d53b51b8ff8acb3dbcf65d9180356b5e66ad7
phantomcoder1996 <reemagody1996@gmail.com>
2023-07-11 06:41:42 +0300

    initial code base

---
+++ src/configs/femaleSpeakers.tsx
@@ -0,0 +1,8 @@
+export const FemaleSpeakers = [
+    {value:"en-GB-SoniaNeural", label:"en-GB-SoniaNeural"},
+    {value:"en-US-JennyNeural", label:"en-US-JennyNeural"},
+    {value:"zh-CN-XiaoxiaoNeural", label:"zh-CN-XiaoxiaoNeural"}
+
+]
+
+export default FemaleSpeakers;
